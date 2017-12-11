89561f91b47ede3cd17afdcc3ac38b45f0675fe4
Bryan Triana <bryantrianarueda@gmail.com>
2017-12-11 16:18:14 -0500

    Add files via upload

--- include/Application.hpp
+++ include/Application.hpp
@@ -1,1 +1,48 @@
+/*
+Copyright (c) 2017 InversePalindrome
+Rampancy - Application.hpp
+InversePalindrome.com
+*/
 
+
+#pragma once
+
+#include "InputManager.hpp"
+
+#include <MYGUI/MyGUI.h>
+#include <MYGUI/MyGUI_OgrePlatform.h>
+
+#include <OGRE/OgreRoot.h>
+#include <OGRE/OgreRenderWindow.h>
+#include <OGRE/OgreSceneManager.h>
+#include <OGRE/OgreCamera.h>
+#include <OGRE/OgreFrameListener.h>
+#include <OGRE/OgreWindowEventUtilities.h>
+
+
+class Application : public Ogre::FrameListener, public Ogre::WindowEventListener
+{
+public:
+	Application();
+	Application(const Application&) = delete;
+	Application& operator=(const Application&) = delete;
+
+	~Application();
+
+	void run();
+
+private:
+	Ogre::Root* root;
+	Ogre::RenderWindow* window;
+	Ogre::SceneManager* sceneManager;
+	Ogre::Camera* camera;
+
+	MyGUI::Gui* gui;
+	MyGUI::OgrePlatform* guiPlatform;
+
+	InputManager inputManager;
+
+	bool shutdown;
+
+	void handleEvents();
+};
---
+++ include/InputManager.hpp
@@ -0,0 +1,42 @@
+/*
+Copyright (c) 2017 InversePalindrome
+Rampancy - InputManager.hpp
+InversePalindrome.com
+*/
+
+
+#pragma once
+
+#include <OGRE/OgreRenderWindow.h>
+
+#include <OIS/OISInputManager.h>
+#include <OIS/OISKeyboard.h>
+#include <OIS/OISMouse.h>
+
+
+class InputManager : public OIS::KeyListener, public OIS::MouseListener
+{
+public:
+	InputManager() = default;
+	InputManager(const InputManager&) = delete;
+	InputManager& operator=(const InputManager&) = delete;
+
+	void initialise(Ogre::RenderWindow* window);
+	~InputManager();
+
+	void capture();
+
+	bool isKeyPressed(OIS::KeyCode keyCode) const;
+
+private:
+	OIS::InputManager* inputManager;
+	OIS::Keyboard* keyboard;
+	OIS::Mouse* mouse;
+
+	virtual bool keyPressed(const OIS::KeyEvent& event) override;
+	virtual bool keyReleased(const OIS::KeyEvent& event) override;
+
+	virtual bool mouseMoved(const OIS::MouseEvent& event) override;
+	virtual bool mousePressed(const OIS::MouseEvent& event, OIS::MouseButtonID id) override;
+	virtual bool mouseReleased(const OIS::MouseEvent& event, OIS::MouseButtonID id) override;
+};
